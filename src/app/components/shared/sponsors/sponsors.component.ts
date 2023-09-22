af8a411337fc4b4274418b927f7db306e9a36167
Luciano Murruni <luciano.murruni@gmail.com>
2023-09-22 16:37:38 +0200

    feat: new sponsor optimization

--- src/app/components/shared/sponsors/sponsors.component.ts
+++ src/app/components/shared/sponsors/sponsors.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 export interface SponsorInterface {
   name: string;
   image: string;
@@ -8,18 +8,18 @@ export interface SponsorInterface {
 @Component({
   selector: 'app-sponsors',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NgOptimizedImage],
   template: ` <div class="mt-36 lg:mt-44">
     <ul
       role="list"
       class="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
     >
       <li *ngFor="let item of mainSponsor">
-        <span
-          class="whitespace-nowrap rounded-full bg-gradient-to-r from-red-600 via-yellow-500 to-red-600 px-2.5 py-0.5 text-sm text-white uppercase"
-        >
-          Main sponsor
-        </span>
+        <div class="flex items-center justify-center w-full">
+          <div class="w-5/12 md:w-3/12 bg-black rounded-t-lg border-4 border-black">
+            <h1 class="text-sm main font-bold uppercase">main PARTNER</h1>
+          </div>
+        </div>
         <ul role="list" class="flex flex-col items-center sm:flex-row">
           <li class="flex place-content-center">
             <div
@@ -37,7 +37,7 @@ export interface SponsorInterface {
                   decoding="async"
                   data-nimg="1"
                   style="color:transparent"
-                  [src]="item.image"
+                  [ngSrc]="item.image"
                 />
               </a>
             </div>
@@ -48,14 +48,14 @@ export interface SponsorInterface {
 
     <ul
       role="list"
-      class="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
+      class="mt-8 flex flex-col md:flex-row items-center justify-center gap-x-8 gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
     >
       <li *ngFor="let item of goldSponsors">
-        <span
-          class="whitespace-nowrap rounded-full bg-gradient-to-r from-yellow-500 via-yellow-400 to-yellow-600 px-2.5 py-0.5 text-sm text-white uppercase"
-        >
-          gold sponsor
-        </span>
+        <div class="flex items-center justify-center w-full">
+          <div class="w-5/12 md:w-3/12 bg-black rounded-t-lg border-4 border-black">
+            <h1 class="text-sm gold font-bold uppercase">GOLD PARTNER</h1>
+          </div>
+        </div>
         <ul
           role="list"
           class="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0"
@@ -76,7 +76,7 @@ export interface SponsorInterface {
                   decoding="async"
                   data-nimg="1"
                   style="color:transparent"
-                  [src]="item.image"
+                  [ngSrc]="item.image"
                 />
               </a>
             </div>
@@ -89,11 +89,11 @@ export interface SponsorInterface {
       class="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
     >
       <li *ngFor="let item of silverSponsor">
-        <span
-          class="whitespace-nowrap rounded-full bg-gradient-to-r from-slate-400 via-slate-200 to-slate-500 px-2.5 py-0.5 text-sm text-white uppercase"
-        >
-          silver sponsor
-        </span>
+        <div class="flex items-center justify-center w-full">
+          <div class="w-5/12 md:w-3/12 bg-black rounded-t-lg border-4 border-black">
+            <h1 class="text-sm silver font-bold uppercase">SILVER PARTNER</h1>
+          </div>
+        </div>
         <ul
           role="list"
           class="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0"
@@ -114,7 +114,7 @@ export interface SponsorInterface {
                   decoding="async"
                   data-nimg="1"
                   style="color:transparent"
-                  [src]="item.image"
+                  [ngSrc]="item.image"
               /></a>
             </div>
           </li>
